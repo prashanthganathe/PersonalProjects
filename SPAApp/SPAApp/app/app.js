902b1587345ceddf7a51ec3a81e654dbcb8aa6c7
prashanthmotifworks <prashanth@motifworks.com>
2014-05-30 10:48:46 +0530

    SPAPP

---
+++ SPAApp/SPAApp/app/app.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="E:\Learning\SPAApp\SPAApp\Scripts/angular.js" />
+(function () {
+    var app = angular.module("FoufsquareApp", ['ngRoute', 'ngResource', 'ui.bootstrap']);
+
+    app.cconfig(function ($routeProvider) {
+
+        $routeProvider.when("/explore", {
+            controller: "placesExplorerController",
+            templateUrl: "/app/views/placeresults.html"
+        });
+
+    });
+}());
